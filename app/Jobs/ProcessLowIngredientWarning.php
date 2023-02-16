e7f1d2bb090073247b04cf61ab3fc240224c6039
adedamola <aadeyinka@coralstone.capital>
2023-02-16 15:37:05 +0100

    add queue to jobs

--- app/Jobs/ProcessLowIngredientWarning.php
+++ app/Jobs/ProcessLowIngredientWarning.php
@@ -12,7 +12,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Notification;
 
-class ProcessLowIngredientWarning// implements ShouldQueue
+class ProcessLowIngredientWarning implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
